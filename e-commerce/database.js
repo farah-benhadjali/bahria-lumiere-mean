21ec78dab7a9e23d0027eb1fc7b95a7ba5a040b4
farah-benhadjali <farah.bhadjali@gmail.com>
2026-04-12 19:11:35 +0100

    fix : use env variable

--- e-commerce/database.js
+++ e-commerce/database.js
@@ -1,5 +1,3 @@
 //mongoose crée une connection entre mangodb et express
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://farahebnhadjali:farah123@cluster0.abexu.mongodb.net/E-commerce')
-.then(db => console.log("database connected"))
-.catch(console.error());
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URI)
